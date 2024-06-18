884afe7e6f2502d009aed6df2de2185ff7008495
Suryaprakash46 <147475316+Suryaprakash46@users.noreply.github.com>
2024-06-18 12:59:11 +0530

    Add files via upload

---
+++ lok.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:32:02 06/18/2024 
+// Design Name: 
+// Module Name:    lok 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module lok(a,b,c);
+input a,b;
+output c;
+assign c=a&b;
+endmodule
